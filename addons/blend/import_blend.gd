e1a0601b55e43ae7484ab0d418c8dce2b3972317
Lyuma <xn.lyuma@gmail.com>
2021-11-10 14:20:13 -0800

    Run blender using command line; pass all import flags into import_scene_from_other_importer. Now supports 3.3

--- addons/blend/import_blend.gd
+++ addons/blend/import_blend.gd
@@ -52,6 +52,14 @@ func _get_import_flags():
 
 
 func _import_scene(path: String, flags: int, bake_fps: int):
+	var import_config_file = ConfigFile.new()
+	import_config_file.load(path + ".import")
+	var compression_flags: int = import_config_file.get_value("params", "meshes/compress", 0)
+	# ARRAY_COMPRESS_BASE = (ARRAY_INDEX + 1)
+	compression_flags = compression_flags << (VisualServer.ARRAY_INDEX + 1)
+	if import_config_file.get_value("params", "meshes/octahedral_compression", false):
+		compression_flags |= VisualServer.ARRAY_FLAG_USE_OCTAHEDRAL_COMPRESSION
+
 	var path_global : String = ProjectSettings.globalize_path(path)
 	path_global = path_global.c_escape()
 	var output_path : String = "res://.import/" + path.get_file() + "-" + path.md5_text() + ".glb"
@@ -60,7 +68,9 @@ func _import_scene(path: String, flags: int, bake_fps: int):
 	var stdout = [].duplicate()
 	var addon_path : String = blender_path
 	var addon_path_global = ProjectSettings.globalize_path(addon_path)
-	var script : String = "import bpy;import os;import sys;bpy.ops.wm.open_mainfile(filepath='GODOT_FILENAME');bpy.ops.export_scene.gltf(filepath='GODOT_EXPORT_PATH',export_format='GLB',export_colors=True,export_all_influences=True,export_extras=True,export_cameras=True,export_lights=True);"
+	var script : String = ("import bpy, os, sys;" +
+		"bpy.ops.wm.open_mainfile(filepath='GODOT_FILENAME');" +
+		"bpy.ops.export_scene.gltf(filepath='GODOT_EXPORT_PATH',export_format='GLB',export_colors=True,export_all_influences=False,export_extras=True,export_cameras=True,export_lights=True);")
 	path_global = path_global.c_escape()
 	script = script.replace("GODOT_FILENAME", path_global)
 	output_path_global = output_path_global.c_escape()
@@ -70,35 +80,23 @@ func _import_scene(path: String, flags: int, bake_fps: int):
 	tex_dir_global.c_escape()
 	dir.make_dir(tex_dir_global)
 	script = script.replace("GODOT_TEXTURE_PATH", tex_dir_global)
-
-	var shell = "sh"
-	var execute_string = "-c"
-
-	var escape_left = "\\\""
-	var escape_right = "\\\""
-
-	if OS.get_name() == "Windows":
-		shell = "cmd.exe"
-		execute_string = "/C"
-		escape_left = "\""
-		escape_right = "\""
-	script = addon_path_global + " --background --python-expr " + escape_left + script + escape_right
-
 	var args = [
-		execute_string,
+		"--background",
+		"--python-expr",
 		script]
 	print(args)
-	var ret = OS.execute(shell, args, true, stdout, true)
+	var ret = OS.execute(addon_path_global, args, true, stdout, true)
 	for line in stdout:
 		print(line)
 	if ret != 0:
 		print("Blender returned " + str(ret))
 		return null
 
-	var gstate : GLTFState = GLTFState.new()
-	var gltf : PackedSceneGLTF = PackedSceneGLTF.new()
-	var root_node : Node = gltf.import_gltf_scene(output_path, 0, 1000.0, 2194432, gstate)
-	root_node.name = path.get_basename().get_file()
+	var root_node: Spatial = null
+	if Engine.get_version_info()["major"] <= 3 and Engine.get_version_info()["minor"] <= 3:
+		root_node = call("import_scene_from_other_importer", output_path, flags, bake_fps)
+	else:
+		root_node = call("import_scene_from_other_importer", output_path, flags, bake_fps, compression_flags)
 	return root_node
 
 
